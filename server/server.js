85014704198088ce98e4b90d2f8427ea770c210e
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2022-03-21 01:23:03 +0530

    app connected to mongodb

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const { chats } = require('./data/data');
+const connectDB = require('./config/db');
 
-dotenv.config()
+dotenv.config();
+connectDB();
 const app = express();
 
 app.get('/api/chats', (req, res) => {
