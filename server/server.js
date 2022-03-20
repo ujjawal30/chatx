61da865b75d277e056c7336c7c847b09799bbfeb
Ujjawal Gupta <ujjawalgupta1999.ug@gmail.com>
2022-03-21 02:43:26 +0530

    login and signup api

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ const express = require('express');
 const dotenv = require('dotenv');
 const { chats } = require('./data/data');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes')
 
 dotenv.config();
+
 connectDB();
 const app = express();
+
+app.use(express.json())
+
+app.use('/api/user', userRoutes)
 
 app.get('/api/chats', (req, res) => {
   res.send(chats)
